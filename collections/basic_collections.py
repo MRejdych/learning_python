77670421b20f3a2adcdfab59581215befd910e55
Marcin <rejdych.marcin.1@gmail.com>
2018-02-06 23:12:24 +0100

    added basic collections example

---
+++ collections/basic_collections.py
@@ -0,0 +1,20 @@
+# list
+
+list = [1, 2, 3, "string", 4.0]
+
+# tuple
+
+tuple = (1, 2, 3)
+
+# dictionary
+
+map = {
+    1: 'one',
+    2: 'two',
+    3: 'three'
+}
+
+# set
+
+set = {1, 2, 3, 4, 5}
+
